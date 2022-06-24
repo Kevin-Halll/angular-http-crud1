e6154f69aa6e029eebc4b73629c6435f18542bbb
SimonDMaxwell <lolicongress@outlook.com>
2022-06-24 17:01:29 -0500

    Menu Interface

--- server/interface.ts
+++ server/interface.ts
@@ -1,9 +1,17 @@
 export interface Products {
-    id: number
-    name: string,
-    description: string,
-    price: string,
-    imageUrl: string,
-    quantitiy: number
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  imageUrl: string;
+  quantity: number;
+}
 
+export interface Menu {
+    id: number,
+    menu_name: string,
+    menu_description: string,
+    menu_size: number,
+    imageUrl: string,
+    cost: string,
 }
