f6ed224a4221dbe133ff0f6e56acfc9be999b013
SimonDMaxwell <lolicongress@outlook.com>
2022-06-24 14:49:36 -0500

    Faker Update, Menu Faked

--- server/fakergen.js
+++ server/fakergen.js
@@ -1,16 +1,24 @@
-const faker = require('faker');
-let database = { products: []}
+const { faker } = require("@faker-js/faker");
 
-for(let i = 1; i < 50; i++ ) {
-    database.products.push({
-        id: i,
-        name: faker.commerce.productName(),
-        description : faker.lorem.sentences(),
-        price : faker.commerce.price(),
-        imageUrl : `https://source.unsplash.com/1600x900/?food?random=${Math.floor(Math.random() * 50)}`,
-        quantity : faker.datatype.number()
+let database = { products: [], menus: [] };
 
-    })
+for (let i = 1; i <= 52; i++) {
+  database.products.push({
+    id: i,
+    name: faker.commerce.product(),
+    description: faker.lorem.sentences(3),
+    price: faker.commerce.price(1, 3000, 2, "$"),
+    imageUrl: faker.image.food(undefined, undefined, true),
+    quantity: faker.datatype.number(),
+  });
+  database.menus.push({
+    id: i,
+    menu_name: faker.commerce.productName(),
+    menu_description: faker.lorem.sentences(3),
+    menu_size: faker.datatype.number(10),
+    imageUrl: faker.image.food(undefined, undefined, true),
+    cost: faker.commerce.price(1, 1000, 2, "$"),
+  });
 }
 
-console.log(JSON.stringify(database ,undefined, 4));
+console.log(JSON.stringify(database, undefined, 2));
